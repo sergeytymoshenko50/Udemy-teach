f279f67f24e13ef271317cfdf34cc8e8ec6bfb4d
Sergey <Sergeytymoshenko50@gmail.com>
2022-06-13 23:45:38 +0300

    цикл з ялинкою

--- main.js
+++ main.js
@@ -1,47 +1,49 @@
-let numberOfFilms = +prompt("How manu movies have you watch?");
+// let numberOfFilms = +prompt("How manu movies have you watch?");
 
-const personalMoviesDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMoviesDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-for(let i = 0; i < 2; i++){
-    let lastFilm = prompt("one of the last watched movies"),
-        filmRating = prompt("how much will you appreciate it");
-    if(lastFilm.length < 50 && lastFilm !== null && lastFilm !== "" && filmRating !== null && filmRating !== ""){
-        personalMoviesDB.movies[lastFilm] = filmRating;
-    }else{
-        i--;
-    }
-}
-if(personalMoviesDB.count <= 10) {
-    console.log("ok");
-}else if(personalMoviesDB.count >= 10 && personalMoviesDB.count <= 30){
-    console.log("good");
-}else if(personalMoviesDB.count >= 30) {
-    console.log("well");
-}else{
-    console.log("error");
-}
-console.log(personalMoviesDB.movies);
+// for(let i = 0; i < 2; i++){
+//     let lastFilm = prompt("one of the last watched movies"),
+//         filmRating = prompt("how much will you appreciate it");
+//     if(lastFilm.length < 50 && lastFilm !== null && lastFilm !== "" && filmRating !== null && filmRating !== ""){
+//         personalMoviesDB.movies[lastFilm] = filmRating;
+//     }else{
+//         i--;
+//     }
+// }
+// if(personalMoviesDB.count <= 10) {
+//     console.log("ok");
+// }else if(personalMoviesDB.count >= 10 && personalMoviesDB.count <= 30){
+//     console.log("good");
+// }else if(personalMoviesDB.count >= 30) {
+//     console.log("well");
+// }else{
+//     console.log("error");
+// }
+// console.log(personalMoviesDB.movies);
 
 
-// не можу додуматись((((((
+// DONE!!!!!!!
 // const lines = 5;
 // let result = '';
+// let count = 1;
+// let gap = lines;
 // for(let i = 0; i <= lines; i ++){
-//     let upI = i;
-//     for(let j = 0; j < (upI + 1);j++){
+//     for(let k = gap; k > 0; k--){
+//         result += " ";
+//     } 
+//     for(let j = 1; j <= count;j++){
 //         result += "*";
-//         if(i === 0){
-//             break;
-//         }
 //     }
-//     upI += 2; 
 //     result += "\n";
+//     gap--;  
+//     count +=2;
 // }
 // console.log(result);
     
