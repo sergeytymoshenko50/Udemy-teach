fce3e4d2ec48f6ab019bd50fba512c6824ca9bd6
Sergey <Sergeytymoshenko50@gmail.com>
2022-06-13 07:48:52 +0300

    third task of second task

---
+++ main.js
@@ -0,0 +1,22 @@
+// const numberOfFilms = prompt("How manu movies have you watch?"),
+// lastFilm = prompt("one of the last watched movies"),
+// filmRating = prompt("how much will you appreciate it");
+        
+
+// const personalMoviesDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// personalMoviesDB.movies[lastFilm] = filmRating;
+
+// console.log(personalMoviesDB.movies);
+for(let i = 2;i <= 10; i++){
+    if(i%2 === 0){
+        continue;
+    };
+    console.log(i);
+};
