d7127369b048462fd90cb5592b73a42f98e5ec33
Sergey <Sergeytymoshenko50@gmail.com>
2022-06-14 23:49:32 +0300

    add function minutes to hours

--- main.js
+++ main.js
@@ -1,32 +1,66 @@
-// let numberOfFilms = +prompt("How manu movies have you watch?");
+let numberOfFilms;
 
-// const personalMoviesDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+function start() {
+    numberOfFilms = +prompt("How manu movies have you watch?");
 
-// for(let i = 0; i < 2; i++){
-//     let lastFilm = prompt("one of the last watched movies"),
-//         filmRating = prompt("how much will you appreciate it");
-//     if(lastFilm.length < 50 && lastFilm !== null && lastFilm !== "" && filmRating !== null && filmRating !== ""){
-//         personalMoviesDB.movies[lastFilm] = filmRating;
-//     }else{
-//         i--;
-//     }
-// }
-// if(personalMoviesDB.count <= 10) {
-//     console.log("ok");
-// }else if(personalMoviesDB.count >= 10 && personalMoviesDB.count <= 30){
-//     console.log("good");
-// }else if(personalMoviesDB.count >= 30) {
-//     console.log("well");
-// }else{
-//     console.log("error");
-// }
-// console.log(personalMoviesDB.movies);
+    while(numberOfFilms == "" || numberOfFilms == null || numberOfFilms == isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How manu movies have you watch?");
+    }
+}
+
+// start();
+
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}; 
+
+function rememberMyFilms() {
+    for(let i = 0; i < 2; i++){
+        let lastFilm = prompt("one of the last watched movies"),
+            filmRating = prompt("how much will you appreciate it");
+        if(lastFilm.length < 50 && lastFilm !== null && lastFilm !== "" && filmRating !== null && filmRating !== ""){
+            personalMoviesDB.movies[lastFilm] = filmRating;
+        }else{
+            i--;
+        }
+    }
+}
+
+// rememberMyFilms();
+
+function detectPersonalLevel(){
+    if(personalMoviesDB.count <= 10) {
+        console.log("ok");
+    }else if(personalMoviesDB.count > 10 && personalMoviesDB.count <= 30){
+        console.log("good");
+    }else if(personalMoviesDB.count >= 30) {
+        console.log("well");
+    }else{
+        console.log("error");
+    }
+}
+
+// detectPersonalLevel();
+
+function showMyDB(checkProperty){
+    if(!checkProperty) {
+        console.log(personalMoviesDB);
+    }
+}
+// showMyDB(personalMoviesDB.privat);
+
+function writeYourGenres() {
+    for(let i = 0; i < 3; i++){
+        personalMoviesDB.genres[i] = prompt(`Уour favorite genre, number ${i+1} ?`);
+    }
+}
+// writeYourGenres();
+
+
 
 
 // DONE!!!!!!!(цикл з ялинкою)
@@ -46,4 +80,24 @@
 //     count +=2;
 // }
 // console.log(result);
-    
+// add another function
+// function getMathResult(arg1, arg2) {
+//     if(arg2 <= 0 && !isNaN(arg2)){
+//         return arg1;
+//     }
+//     let resStr = `${arg1}`;
+//     let sum = arg1;
+//     for(let i = 1; i < arg2; i++){
+//         sum += arg1;
+//         resStr += `---${sum}`;
+//     }
+//     return resStr;
+// }   
+// console.log(getMathResult(3, "df"));
+
+function getTimeFromMinutes(minutes) {
+    const hours = parseInt(minutes / 60);
+    // let result = `Єто ${} ${} и ${} ${}`;
+    return hours;
+}
+console.log(getTimeFromMinutes(60));
